b363923e4ba772c612c55005eaedde0a2e0abe44
Super Sonic <systemfrontier9@gmail.com>
2020-05-20 13:40:52 +0800

    Added a toggle feature in the javascript class.

--- js/list.js
+++ js/list.js
@@ -1,34 +1,106 @@
-export const List = {
-    init(list) {
-        this.el = document.createElement('li');
-        this.el.classList = 'col s12';
-        this.container = list;
-    },
+export default class List {
+    constructor(container) {
+        this.container = container;
+        this.LIST = null;
+        this.ID = null;
 
-    html(options) {
-        return `
-            <p>
-                ${options.task}
-            </p>
-            <button class="btn-small blue waves-effect waves-light done">
-                Done <i class="material-icons right">check</i>
-            </button>
-            <button class="btn-small red waves-effect waves-light remove">
+        const data = localStorage.getItem('TASK');
+
+        if (data) {
+            this.LIST = JSON.parse(data);
+            this.ID = this.LIST.length;
+            this.loadList(this.LIST);
+        } else {
+            this.LIST = [];
+            this.ID = 0;
+        }
+    }
+
+    addList(id, task, check, remove) {
+        if (remove) {
+            return;
+        }
+        const LINE = check ? 'check' : 'uncheck';
+        const CHECK = check
+            ? `
+                <button
+                    class="btn-small orange waves-effect waves-light right"
+                >
+                    Undone <i class="material-icons left">check_box</i>
+                </button>`
+            : `
+                <button
+                    class="btn-small blue waves-effect waves-light left"
+                >
+                    Done <i class="material-icons right">check_box_outline_blank</i>
+                </button>`;
+
+        const LI = document.createElement('li');
+        LI.className = 'col s12';
+        LI.setAttribute('data-id', `${id}`);
+        LI.setAttribute('data-task', `${task}`);
+        LI.setAttribute('data-check', `${check}`);
+        LI.setAttribute('data-remove', `${remove}`);
+
+        LI.innerHTML = `
+            <p class="${LINE}">${task}</p>
+            ${CHECK}
+            <button
+                class="btn-small red waves-effect waves-light right"
+            >
                 Remove
                 <i class="material-icons right">delete_forever</i>
-            </button>
-        `;
-    },
+            </button>`;
 
-    addList(options = {}) {
-        options = Object.assign(
-            {
-                task: null,
-            },
-            options
-        );
+        this.container.insertBefore(LI, this.container.children.lastChild);
+    }
 
-        this.el.innerHTML = this.html(options);
-        this.container.insertBefore(this.el, this.container.children[0]);
-    },
-};
+    loadList(array) {
+        array.forEach((key) => {
+            this.addList(key.id, key.task, key.check, key.remove);
+        });
+    }
+
+    toggleTask(element) {
+        const data = localStorage.getItem('TASK');
+        const LIST = JSON.parse(data);
+        const parent = element.target.parentNode;
+        if (
+            element.target.tagName === 'BUTTON' &&
+            parent.hasAttribute('data-check')
+        ) {
+            const check = JSON.parse(parent.attributes['data-check'].value)
+                ? true
+                : false;
+
+            if (check) {
+                element.target.classList.replace('orange', 'blue');
+                element.target.innerHTML =
+                    'DONE <i class="material-icons right">check_box_outline_blank<i>';
+                parent.setAttribute('data-check', 'false');
+                element.target.previousElementSibling.setAttribute(
+                    'class',
+                    'uncheck'
+                );
+                LIST[parent.dataset.id].check = JSON.parse(
+                    parent.dataset.check
+                );
+            } else {
+                element.target.classList.replace('blue', 'orange');
+                element.target.innerHTML =
+                    'UNDONE <i class="material-icons right">check_box<i>';
+                parent.setAttribute('data-check', 'true');
+                element.target.previousElementSibling.setAttribute(
+                    'class',
+                    'check'
+                );
+                LIST[parent.dataset.id].check = JSON.parse(
+                    parent.dataset.check
+                );
+            }
+            console.log('CHECK UPDATE:', LIST[JSON.parse(parent.dataset.id)]);
+            localStorage.setItem('TASK', JSON.stringify(LIST));
+            console.log(localStorage.getItem('TASK'));
+        }
+    }
+}
--- js/main.js
+++ js/main.js
@@ -1,24 +1,33 @@
-import { Modal } from './modal.js';
-import { Toast } from './toast.js';
-import { List } from './list.js';
+'use strict';
+import List from './List.js';
 
-document.addEventListener('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', (e) => {
+    const modalElems = document.querySelectorAll('.modal');
+    M.Modal.init(modalElems);
     const add = document.querySelector('#add-list');
-    const list = document.querySelector('#my-list');
+    const container = document.querySelector('#my-list');
+    const input = document.querySelector('#data');
+    const list = new List(container);
 
-    Modal.init();
-    Toast.init();
-    List.init(list);
-
-    add.addEventListener('click', (e) => Modal.show());
-    list.addEventListener('click', (e) => {
-        Toast.show({
-            class: 'show',
-            title: 'Success title.',
-            message: 'Success message!',
-        });
-        List.addList({ task: 'This is some sample text.' });
+    input.addEventListener('keyup', (e) => {
+        if (input.value) {
+            if (e.which === 13) {
+                list.addList(list.ID, input.value, false, false);
+                list.LIST.push({
+                    id: list.ID,
+                    task: input.value,
+                    check: false,
+                    remove: false,
+                });
+                localStorage.setItem('TASK', JSON.stringify(list.LIST));
+                input.value = '';
+                list.ID++;
+                M.Modal.init(modalElems, 'close');
+            }
+        }
     });
+
+    container.addEventListener('click', list.toggleTask);
 });
 
 // document.body.addEventListener('DOMSubtreeModified', (event) => {
